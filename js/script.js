64378bd145137b7a5836bda5f2d06ab797445dac
Arianna Carotti <arianna.carotti@gmail.com>
2021-04-20 14:53:20 +0200

    creato html e css

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Slider. Al click sulla sulle frecce rendiamo visibile l'immagine successiva o precedente (in base alla freccia sulla quale ho cliccato).
+// Lo slider non deve mai rompersi, ma se l'immagine visibile è l'ultima e clicco sulla freccia a destra lo slider mostra la prima.
+// Se l'immagine visibile è la prima e clicco sulla freccia a sinistra, lo slider mostra l'ultima.
+
+// Bonus:
+// Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+
+
+var app = new Vue(
+    {
+        el: '#root',
+    
+        data: {
+
+
+        },
+        methods: {
+
+        }
+
+    }
+
+)
